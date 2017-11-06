74f4e062f0632c603695a3a72889fcd4d612d72a
lao.tseu.is.alive@gmail.com <lao.tseu.is.alive@gmail.com>
2017-11-06 14:48:12 +0100

    added button to launch applyFilter, and timer counters on the console

--- app/worker.js
+++ app/worker.js
@@ -1,13 +1,14 @@
 
 addEventListener('message', (d) => {
     //TODO check how to pass parameter with Typed Array in worker.postMessage
-    const redFactor = 1
-    const greenFactor = 0.1
-    const  blueFactor = 0.51
+    const redFactor = 1.1
+    const greenFactor = 0.5
+    const  blueFactor = 1.1
     const img = d.data
     const w = img.width
     const h = img.height
     console.log('...inside webworker ...', d)
+    let t1 = performance.now()
     for (let y = 0; y < h; y++) { // for every row of pixel
         for (let x = 0; x < w; x++) { // for every pixel in column
             let index = (x + y * w) * 4 // RGBA bytes
@@ -22,6 +23,8 @@ addEventListener('message', (d) => {
             img.data[index + 3] = img.data[index + 3]
         }
     }
+    let t2 = performance.now()
+    console.log("---> double y,x loop to modifiy img took " + Math.round(t2 - t1) + " milliseconds.")
     console.log('<--sending back postMessage from webworker to mainthread')
     postMessage(img, [img.data.buffer])  // let send back result
 })
